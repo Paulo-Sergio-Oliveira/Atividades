689a9da1822fc30bb42ff9919aaefbf90b1ffd46
Paulo-Sergio-Oliveira <99352391+Paulo-Sergio-Oliveira@users.noreply.github.com>
2022-03-30 08:35:17 -0300

    Update Elevador.java

--- Resoluções/Aula 01/Elevador.java
+++ Resoluções/Aula 01/Elevador.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
 public class Elevador {
+    Scanner Read = new Scanner(System.in);
     Double altura;
     Double largura;
     Double profundidade;
     int capacidade;
 
     public void funcao(){
-        System.out.println("Foi para o andar selecionado!");
+        int andar = Read.nextInt();
+        do {
+            for (int cont = 1; cont <= andar; cont++){
+                System.out.println("Andar "+cont);
+                if (cont == andar) {
+                    System.out.println("Você chegou no seu andar!");
+                }
+            }
+            System.out.println("Quer continuar? S/N");
+            String R1;
+            if (R1 == "S"); {
+                
+            }
+            Read.close();
+        } while (resposta == "s");
     }
     public void impDados(){
         System.out.println("Altura: "+this.altura+" m");
