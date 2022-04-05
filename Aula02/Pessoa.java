586c79f2ee14cda8ccdb8a095f19cf9e6cd73e86
Paulo-Sergio-Oliveira <99352391+Paulo-Sergio-Oliveira@users.noreply.github.com>
2022-04-05 09:48:48 -0300

    Update Pessoa.java

--- Aula02/Pessoa.java
+++ Aula02/Pessoa.java
@@ -1,27 +1,45 @@
+import javax.swing.JOptionPane;
+
 public class Pessoa {
     private String nome;
-    private int rg;
-    private int cpf;
+    private String rg;
+    private String cpf;
     private int idade;
     private String sexo;
     private String escivil;
     private String endereço;
-    private int telefone;
+    private String telefone;
     private int tieleitor;
 
+    public Pessoa(){
+
+    }
+
+    public Pessoa(String nome, String rg, String cpf, int idade, String sexo, String escivil, String endereço,
+            String telefone, int tieleitor) {
+        this.nome = nome;
+        this.rg = rg;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.escivil = escivil;
+        this.endereço = endereço;
+        this.telefone = telefone;
+        this.tieleitor = tieleitor;
+    }
     public String getNome() {
         return nome;
     }
     public int getTieleitor() {
         return tieleitor;
     }
-    public int getCpf() {
+    public String getCpf() {
         return cpf;
     }
-    public int getRg() {
+    public String getRg() {
         return rg;
     }
-    public int getTelefone() {
+    public String getTelefone() {
         return telefone;
     }
     public String getEndereço() {
@@ -36,31 +54,8 @@ public class Pessoa {
     public int getIdade() {
         return idade;
     }
-    public void setTieleitor(int tieleitor) {
-        this.tieleitor = tieleitor;
-    }
-    public void setTelefone(int telefone) {
-        this.telefone = telefone;
-    }
-    public void setEndereço(String endereço) {
-        this.endereço = endereço;
-    }
-    public void setEscivil(String escivil) {
-        this.escivil = escivil;
-    }
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    public void setRg(int rg) {
-        this.rg = rg;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public void setCpf(int cpf) {
-        this.cpf = cpf;
+    public void impDados(){
+        JOptionPane.showMessageDialog(null, "Pessoa [cpf=" + cpf + ", endereço=" + endereço + ", escivil=" + escivil + ", idade=" + idade + ", nome="
+        + nome + ", rg=" + rg + ", sexo=" + sexo + ", telefone=" + telefone + ", tieleitor=" + tieleitor + "]", "Resposta", 1);
     }
 }
