3cf2b172e928fd1ca10ed4c89d13c115e5e308b0
Paulo-Sergio-Oliveira <99352391+Paulo-Sergio-Oliveira@users.noreply.github.com>
2022-04-05 12:29:57 -0300

    Update MainClass.java

--- Aula02/MainClass.java
+++ Aula02/MainClass.java
@@ -7,20 +7,14 @@ public class MainClass {
         "Fomento, Iguatu - CE","88 93153-2722",235621387);
         P2.impDados();
         Pessoa P3 = new Pessoa("Vinicio","75432843-6","856.497.957-34",15,"Masculino","Casado",
-        "FBairro Areias, Iguatu - CE","88 96376-8623",678217638);
+        "Bairro Areias, Iguatu - CE","88 96376-8623",678217638);
         P3.impDados();
         Pessoa P4 = new Pessoa("Thierry","235448334-7","821.573.155-98",16,"Masculino","Casado",
         "Altiplano, Iguatu - CE","88 92154-2842",923647862);
         P4.impDados();
-        Pessoa P5 = new Pessoa();
-        P5.setNome("Herick Levi");
-        P5.setCpf("721.563.234-25");
-        P5.setRg("23648726-4");
-        P5.setIdade(16);
-        P5.setEscivil("Solteiro");
-        P5.setSexo("Masculino");
-        P5.setTelefone("88 92386-7823");
-        P5.setTieleitor(621782136);
-        P5.setEndereço("13 de Maio, Iguatu - CE");
+        Pessoa P5 = new Pessoa("Herick Levi","23648726-4","721.563.234-25",16,"Masculino","Solteiro",
+        "13 de Maio, Iguatu - CE","88 92386-7823",621782136);
+        P5.impDados();
     }
 }
+// Paulo Sergio Vieira de Oliveira
